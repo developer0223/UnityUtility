5843c9b1b01e96f930f563e8e2da434b4cad02bf
JungukHom <developer0223@gmail.com>
2022-02-10 11:13:47 +0900

    Add | Extension methods

--- Project/UnityUtility/Assets/Extensions/UnityExtension.Transform.cs
+++ Project/UnityUtility/Assets/Extensions/UnityExtension.Transform.cs
@@ -19,4 +19,25 @@ public static partial class UnityExtension
             GameObject.Destroy(parent.GetChild(i).gameObject);
         }
     }
+
+    public static void DestroyChildren<T>(this Transform parent) where T : Component
+    {
+        T[] components = parent.GetComponentsInChildren<T>();
+        for (int i = components.Length - 1; i >= 0; i--)
+        {
+            GameObject.Destroy(components[i].gameObject);
+        }
+    }
+
+    public static void DestroyChildren(this Transform parent, string containsName)
+    {
+        for (int i = parent.childCount - 1; i >= 0; i--)
+        {
+            Transform current = parent.GetChild(i);
+            if (current.gameObject.name.Contains(containsName))
+            {
+                GameObject.Destroy(current.gameObject);
+            }
+        }
+    }
 }
