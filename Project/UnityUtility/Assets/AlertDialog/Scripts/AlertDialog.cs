cad88864c9fb377e9f976dc8e19e09a1326417e5
developer0223 <developer0223@gmail.com>
2022-06-14 12:04:46 +0900

    AlertDialog | Add | Alertdialog script

--- Project/UnityUtility/Assets/AlertDialog/Scripts/AlertDialog.cs
+++ Project/UnityUtility/Assets/AlertDialog/Scripts/AlertDialog.cs
@@ -13,17 +13,29 @@ using UnityEngine.Events;
 
 public class AlertDialog : MonoBehaviour
 {
-    private static readonly string Path = "/UI/AlertDialog/AlertDialog";
+    private static readonly string Path = "UI/Dialog/AlertDialog";
+
+    [Header("Wrapper")]
+    public Image img_wrapper = null;
+
+    [Header("Content")]
+    public RectTransform rect_tr_content_area = null;
+
+    [Header("Backgrounds")]
+    public Image img_title_background = null;
+    public Image img_content_background = null;
 
     [Header("Texts")]
     public Text txt_title = null;
     public Text txt_content = null;
 
-    [Header("Buttons")]
-    public Button btn_ok = null;
+    [Header("Button (Cancel)")]
     public Button btn_cancel = null;
-    public Text txt_button_ok = null;
     public Text txt_button_cancel = null;
+
+    [Header("Button (OK)")]
+    public Button btn_ok = null;
+    public Text txt_button_ok = null;
 
     private static AlertDialog Create()
     {
@@ -37,11 +49,6 @@ public class AlertDialog : MonoBehaviour
         return _script;
     }
 
-    private void Awake()
-    {
-        btn_cancel.gameObject.SetActive(false);
-    }
-
     public class Builder
     {
         private AlertDialog dialog = null;
@@ -49,6 +56,7 @@ public class AlertDialog : MonoBehaviour
         public Builder()
         {
             dialog = Create();
+            SetCancelButtonDisalbed();
         }
 
         public Builder(AlertDialog dialog)
@@ -57,6 +65,35 @@ public class AlertDialog : MonoBehaviour
                 GameObject.Destroy(dialog);
 
             this.dialog = dialog;
+        }
+
+        #region Wrapper Area
+        public Builder SetWrapperImageEnabled(bool enable)
+        {
+            dialog.img_wrapper.gameObject.SetActive(enable);
+            return this;
+        }
+
+        public Builder SetWrapperImageColor(Color newColor)
+        {
+            dialog.img_wrapper.color = newColor;
+            return this;
+        }
+
+        public Builder SetWrapperImageAlpha(float alpha01)
+        {
+            Color newColor = dialog.img_wrapper.color;
+            newColor.a = alpha01;
+            dialog.img_wrapper.color = newColor;
+            return this;
+        }
+        #endregion
+
+        #region Title Area
+        public Builder SetTitleBarBackgroundColor(Color newColor)
+        {
+            dialog.img_title_background.color = newColor;
+            return this;
         }
 
         public Builder SetTitleText(string title)
@@ -77,6 +114,20 @@ public class AlertDialog : MonoBehaviour
             return this;
         }
 
+        public Builder SetTitleTextStyle(FontStyle newFontStyle)
+        {
+            dialog.txt_title.fontStyle = newFontStyle;
+            return this;
+        }
+        #endregion
+
+        #region Content Area
+        public Builder SetContentBackgroundColor(Color newColor)
+        {
+            dialog.img_content_background.color = newColor;
+            return this;
+        }
+
         public Builder SetContentText(string content)
         {
             dialog.txt_content.text = content;
@@ -89,12 +140,20 @@ public class AlertDialog : MonoBehaviour
             return this;
         }
 
+        public Builder SetContentTextFontStyle(FontStyle newFontStyle)
+        {
+            dialog.txt_content.fontStyle = newFontStyle;
+            return this;
+        }
+
         public Builder SetContentColor(Color newColor)
         {
             dialog.txt_content.color = newColor;
             return this;
         }
+        #endregion
 
+        #region Button Area (OK)
         public Builder SetOkButton(string text, UnityAction callback)
         {
             dialog.txt_button_ok.text = text;
@@ -115,14 +174,23 @@ public class AlertDialog : MonoBehaviour
             return this;
         }
 
+        public Builder SetOkButtonTextFontStyle(FontStyle newFontStyle)
+        {
+            dialog.txt_button_ok.fontStyle = newFontStyle;
+            return this;
+        }
+
         public Builder SetOkButtonBackgroundColor(Color newColor)
         {
             dialog.btn_ok.image.color = newColor;
             return this;
         }
+        #endregion
 
+        #region Button Area (Cancel)
         public Builder SetCancelButton(string text, UnityAction callback)
         {
+            SetCancelButtonEnabled();
             dialog.txt_button_cancel.gameObject.SetActive(true);
             dialog.txt_button_cancel.text = text;
             dialog.btn_cancel.onClick.RemoveAllListeners();
@@ -142,6 +210,12 @@ public class AlertDialog : MonoBehaviour
             return this;
         }
 
+        public Builder SetCancelButtonTextFontStyle(FontStyle newFontStyle)
+        {
+            dialog.txt_button_cancel.fontStyle = newFontStyle;
+            return this;
+        }
+
         public Builder SetCancelButtonBackgroundColor(Color newColor)
         {
             dialog.btn_cancel.image.color = newColor;
@@ -152,6 +226,31 @@ public class AlertDialog : MonoBehaviour
         {
             dialog.gameObject.SetActive(true);
             return this;
+        }
+        #endregion
+
+        private void SetCancelButtonEnabled()
+        {
+            dialog.btn_cancel.gameObject.SetActive(true);
+
+            float width = dialog.rect_tr_content_area.rect.width;
+            float height = dialog.txt_button_ok.rectTransform.rect.height;
+
+            Debug.Log($"SetCancelButtonEnabled. width: {width}, height: {height}");
+
+            dialog.btn_ok.GetComponent<RectTransform>().sizeDelta = new Vector2(width / 2, height);
+            dialog.btn_cancel.GetComponent<RectTransform>().sizeDelta = new Vector2(width / 2, height);
+        }
+
+        private void SetCancelButtonDisalbed()
+        {
+            float width = dialog.rect_tr_content_area.rect.width;
+            float height = dialog.txt_button_ok.rectTransform.rect.height;
+
+            Debug.Log($"SetCancelButtonDisalbed. width: {width}, height: {height}");
+
+            dialog.btn_cancel.gameObject.SetActive(false);
+            dialog.btn_ok.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
         }
     }
 }
