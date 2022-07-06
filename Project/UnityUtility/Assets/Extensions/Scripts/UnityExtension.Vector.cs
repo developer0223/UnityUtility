d719fbdebf46d847dd59625706f333e306b52069
developer0223 <developer0223@gmail.com>
2022-07-06 18:17:39 +0900

    Edit | UnityExtension.Vector

--- Project/UnityUtility/Assets/Extensions/Scripts/UnityExtension.Vector.cs
+++ Project/UnityUtility/Assets/Extensions/Scripts/UnityExtension.Vector.cs
@@ -14,6 +14,12 @@ public static partial class UnityExtension
     public static Vector3 WithY(this Vector3 v, float y) => new Vector3(v.x, y, v.z);
 
     public static Vector3 WithZ(this Vector3 v, float z) => new Vector3(v.x, v.y, z);
+
+    public static Vector3 WithXY(this Vector3 v, float x, float y) => new Vector3(x, y, v.z);
+
+    public static Vector3 WithYZ(this Vector3 v, float y, float z) => new Vector3(v.x, y, z);
+
+    public static Vector3 WithXZ(this Vector3 v, float x, float z) => new Vector3(x, v.y, z);
     #endregion
 
     #region Vector2
