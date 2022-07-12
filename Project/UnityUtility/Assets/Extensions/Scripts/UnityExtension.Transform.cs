ae4d1057f6352a6422b1b0bf171038c012a2751a
developer0223 <developer0223@gmail.com>
2022-07-12 18:17:53 +0900

    Edit | UnityExtension.Transform

--- Project/UnityUtility/Assets/Extensions/Scripts/UnityExtension.Transform.cs
+++ Project/UnityUtility/Assets/Extensions/Scripts/UnityExtension.Transform.cs
@@ -6,6 +6,24 @@ using UnityEngine;
 /// </summary>
 public static partial class UnityExtension
 {
+    public static void DeActivateAllChildren(this Transform parent)
+    {
+        for (int i = parent.childCount - 1; i >= 0; i--)
+        {
+            parent.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+    public static void DeActivateChildrenWith<T>(this Transform parent, bool includeInactive = false) where T : Component
+    {
+        T[] components = parent.GetComponentsInChildren<T>(includeInactive);
+        for (int i = components.Length - 1; i >= 0; i--)
+        {
+            components[i].gameObject.SetActive(false);
+        }
+    }
+
+
     public static void DestroyAllChildren(this Transform parent)
     {
         for (int i = parent.childCount - 1; i >= 0; i--)
@@ -14,16 +32,16 @@ public static partial class UnityExtension
         }
     }
 
-    public static void DestroyChildren<T>(this Transform parent) where T : Component
+    public static void DestroyChildrenWith<T>(this Transform parent, bool includeInactive = false) where T : Component
     {
-        T[] components = parent.GetComponentsInChildren<T>();
+        T[] components = parent.GetComponentsInChildren<T>(includeInactive);
         for (int i = components.Length - 1; i >= 0; i--)
         {
             GameObject.Destroy(components[i].gameObject);
         }
     }
 
-    public static void DestroyChildren(this Transform parent, string containsName)
+    public static void DestroyChildrenContainsName(this Transform parent, string containsName)
     {
         for (int i = parent.childCount - 1; i >= 0; i--)
         {
