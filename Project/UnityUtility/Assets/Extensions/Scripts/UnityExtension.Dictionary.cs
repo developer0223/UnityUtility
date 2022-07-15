8fc022fc16cc0201a1b110ec837279af38527023
developer0223 <developer0223@gmail.com>
2022-07-15 10:41:54 +0900

    Edit | UnityExtension.Dictionary

--- Project/UnityUtility/Assets/Extensions/Scripts/UnityExtension.Dictionary.cs
+++ Project/UnityUtility/Assets/Extensions/Scripts/UnityExtension.Dictionary.cs
@@ -6,11 +6,15 @@ using System.Collections.Generic;
 /// </summary>
 public static partial class UnityExtension
 {
-    //public static bool TryGetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> thiz, TKey key, out TValue value)
-    //{
-    //    if (thiz.ContainsKey(key))
-    //    {
+    public static bool TryGetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> thiz, TKey key, out TValue value)
+    {
+        if (thiz.ContainsKey(key))
+        {
+            value = thiz[key];
+            return true;
+        }
 
-    //    }
-    //}
+        value = default;
+        return false;
+    }
 }
