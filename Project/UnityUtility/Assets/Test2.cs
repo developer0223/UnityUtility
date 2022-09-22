c5d6df6a6e37b8e34d7aea31ebf28c27b3071649
developer0223 <developer0223@gmail.com>
2022-09-22 10:09:13 +0900

    Add | Awake, Start Test code

---
+++ Project/UnityUtility/Assets/Test2.cs
@@ -0,0 +1,24 @@
+// System
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Unity
+using UnityEngine;
+using UnityEngine.UI;
+
+// Project
+// Alias
+
+public class Test2 : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log($"Test2.Awake");
+    }
+
+    private void Start()
+    {
+        Debug.Log($"Test2.Start");
+    }
+}
